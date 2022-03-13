0de7913d4cc8ecb89759d3b0235eba349bc69a91
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-03-12 23:21:23 -0500

    Added make_move() that handles won and lost games

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -17,4 +17,13 @@ defmodule HangmanImplGameTest do
     assert game.game_state == :initializing
     assert game.letters == ["h", "e", "l", "l", "o"]
   end
+
+  test "State doesn't change if game is won or lost" do
+    for state <- [:won, :lost] do
+      game = Game.new_game("hello")
+      game = Map.put(game, :game_state, state)
+      {new_game, _tally} = Game.make_move(game, "dummy")
+      assert new_game == game
+    end
+  end
 end
