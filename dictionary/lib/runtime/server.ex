946c350754d6df4724da5f71af526f6e96bd56b3
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-03-20 22:22:26 -0400

    Converted the Dictionary module to an application

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,15 +1,17 @@
 defmodule Dictionary.Runtime.Server do
   @type t :: pid()
 
+  @me __MODULE__
+
   alias Dictionary.Impl.WordList
 
   @spec start_link :: {:error, any} | {:ok, pid}
   def start_link() do
-    Agent.start_link(&WordList.word_list/0)
+    Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
-  @spec random_word(pid) :: String.t()
-  def random_word(pid) do
-    Agent.get(pid, &WordList.random_word/1)
+  @spec random_word() :: String.t()
+  def random_word() do
+    Agent.get(@me, &WordList.random_word/1)
   end
 end
