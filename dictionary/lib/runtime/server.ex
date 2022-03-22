981a32474c44a79dd3921b64e49b84e087365176
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-03-22 17:17:34 -0400

    Removed the code that caused random crashes on purpose

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -14,11 +14,6 @@ defmodule Dictionary.Runtime.Server do
 
   @spec random_word() :: String.t()
   def random_word() do
-    # Code to test the supervision (crash the agent randomly in 1/3 of cases)
-    if :rand.uniform() < 0.33 do
-      Agent.get(@me, fn _ -> exit(:boom) end)
-    end
-
     Agent.get(@me, &WordList.random_word/1)
   end
 end
