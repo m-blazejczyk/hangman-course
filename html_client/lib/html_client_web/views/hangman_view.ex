a4bc9fe37ace40c59ba92d1cde1b608b720791d1
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-03-29 15:54:18 -0400

    Improved styling, added a Play Again button etc.

--- html_client/lib/html_client_web/views/hangman_view.ex
+++ html_client/lib/html_client_web/views/hangman_view.ex
@@ -1,6 +1,29 @@
 defmodule HtmlClientWeb.HangmanView do
   use HtmlClientWeb, :view
 
+  @state_fields %{
+    initializing: {"initializing", "Please guess the first letter"},
+    good_guess: {"good-guess", "Good guess!"},
+    bad_guess: {"bad-guess", "Sorry, that's a bad guess  :("},
+    won: {"won", "You won!"},
+    lost: {"lost", "Sorry, you lost…  :("},
+    already_used: {"already-used", "You already used that letter"}
+  }
+  def move_status(state) do
+    {class, message} = @state_fields[state]
+    "<div class='status #{class}'>#{message}</div>"
+  end
+
+  def continue_or_try_again(conn, state) when state in [:won, :lost] do
+    button("Try again", to: Routes.hangman_path(conn, :new))
+  end
+
+  def continue_or_try_again(conn, _state) do
+    form_for(conn, Routes.hangman_path(conn, :update), [as: "make_move", method: :put], fn f ->
+      [text_input(f, :guess), " ", submit("Make next guess")]
+    end)
+  end
+
   def figure_for(0) do
     ~s{
       ┌───┐
