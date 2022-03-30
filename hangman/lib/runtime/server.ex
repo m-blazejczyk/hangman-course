56ff7ba717948b800eba7b68cb429f10d72e33a1
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-03-29 22:11:09 -0400

    Added a watchdog to kill game servers after some time of inactivity

--- hangman/lib/runtime/server.ex
+++ hangman/lib/runtime/server.ex
@@ -1,8 +1,12 @@
 defmodule Hangman.Runtime.Server do
   alias Hangman.Impl.Game
+  alias Hangman.Runtime.Watchdog
   use GenServer
 
   @type t :: pid
+
+  # 1 hour
+  @idle_timeout 1 * 60 * 1000
 
   ### client process
 
@@ -13,15 +17,18 @@ defmodule Hangman.Runtime.Server do
   ### server process
 
   def init(_) do
-    {:ok, Game.new_game()}
+    watcher = Watchdog.start(@idle_timeout)
+    {:ok, {Game.new_game(), watcher}}
   end
 
-  def handle_call({:make_move, guess}, _from, game) do
+  def handle_call({:make_move, guess}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
     {updated_game, tally} = Game.make_move(game, guess)
-    {:reply, tally, updated_game}
+    {:reply, tally, {updated_game, watcher}}
   end
 
-  def handle_call({:tally}, _from, game) do
-    {:reply, Game.tally(game), game}
+  def handle_call({:tally}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
+    {:reply, Game.tally(game), {game, watcher}}
   end
 end
